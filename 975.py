bea5df6a90e4f3ca89586a08596e4a527293cea3
Gavin Peng <gavinsweden@gmail.com>
2019-06-04 15:30:13 +0100

    0406

--- 975.py
+++ 975.py
@@ -4,56 +4,26 @@ import bisect
 
 class Solution:
     def oddEvenJumps(self, A: List[int]) -> int:
-        E, O = [False] * len(A), [False] * len(A)
-        E[-1], O[-1] = True, True
-        s = [(A[-1], len(A) - 1)]
-        for i in range(len(A) - 2, -1, -1):
-            idx = bisect.bisect_left(s, (A[i], i))
-            s.insert(idx, (A[i], i))
-            if idx == 0:
-                if s[idx + 1][0] == s[idx][0]:
-                    if E[s[idx + 1][1]]:
-                        O[i] = True
-                    if O[s[idx + 1][1]]:
-                        E[i] = True
-                else:
-                    if E[s[idx + 1][1]]:
-                        O[i] = True
-            elif idx == len(s) - 1:
-                if idx - 2 > -1 and s[idx - 2][0] == s[idx - 1][0]:
-                    newIndex = idx - 1
-                    while newIndex > 0 and s[newIndex - 1][0] == s[newIndex][0]:
-                        newIndex -= 1
-                    if E[s[newIndex][1]]:
-                        O[i] = True
-                    if O[s[newIndex][1]]:
-                        E[i] = True
-                else:
-                    if O[s[idx - 1][1]]:
-                        E[i] = True
-            else:
-                if idx - 2 > -1 and s[idx - 2][0] == s[idx - 1][0]:
-                    newIndex = idx - 1
-                    while newIndex > 0 and s[newIndex - 1][0] == s[newIndex][0]:
-                        newIndex -= 1
-                    if E[s[newIndex][1]]:
-                        O[i] = True
-                    if O[s[newIndex][1]]:
-                        E[i] = True
-                elif s[idx + 1][0] == s[idx][0]:
-                    if E[s[idx + 1][1]]:
-                        O[i] = True
-                    if O[s[idx + 1][1]]:
-                        E[i] = True
-                else:
-                    if E[s[idx + 1][1]]:
-                        O[i] = True
-                    if O[s[idx - 1][1]]:
-                        E[i] = True
-            print(s)
-            print(O)
-            print(E)
-        return sum(O)
+        N = len(A)
+        def makeNext(idx):
+            ans = [None] * N
+            stack = []
+            for i in idx:
+                while stack and i > stack[-1]:
+                    ans[stack.pop()] = i
+                stack.append(i)
+            return ans
+        oddNext = makeNext(sorted(range(N), key=lambda x:A[x]))
+        evenNext = makeNext(sorted(range(N), key=lambda x:-A[x]))
+        odd, even = [False] * N, [False] * N
+        odd[-1], even[-1] = True, True
+        for i in range(N - 2, -1, -1):
+            if oddNext[i] and even[oddNext[i]]:
+                odd[i] = True
+            if evenNext[i] and odd[evenNext[i]]:
+                even[i] = True
+        return sum(odd)
+
 
 
 
