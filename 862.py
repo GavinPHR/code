c67aa7a5b9c481575f34b47be3f9c028e2e80ff0
Gavin Peng <gavinsweden@gmail.com>
2019-08-12 15:07:35 +0100

    1208

--- 862.py
+++ 862.py
@@ -1,6 +1,21 @@
 # Shortest Subarray with Sum at Least K
 from typing import List
-
+import collections
 
 class Solution:
-    def shortestSubarray(self, A: List[int], K: int) -> int:
+    def shortestSubarray(self, A, K):
+        N = len(A)
+        B = [0] * (N + 1)
+        for i in range(N): B[i + 1] = B[i] + A[i]
+        d = collections.deque()
+        res = N + 1
+        for i in range(N + 1):
+            while d and B[i] - B[d[0]] >= K: res = min(res, i - d.popleft())
+            while d and B[i] <= B[d[-1]]: d.pop()
+            d.append(i)
+        return res if res <= N else -1
+
+
+s = Solution()
+a= [-34,37,51,3,-12,-50,51,100,-47,99,34,14,-13,89,31,-14,-44,23,-38,6]
+print(s.shortestSubarray(a,151))
