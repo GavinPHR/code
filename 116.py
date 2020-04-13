36b0fc5dab20aa600a0744eca6726ffc212401c2
Gavin Peng <gavinsweden@gmail.com>
2020-04-13 11:27:28 +0100

    1304

---
+++ 116.py
@@ -0,0 +1,24 @@
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root: return 
+        next = root.left
+        cur = root
+        while cur.left:
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
+                cur = cur.next
+            else:
+                cur = next
+                next = cur.left
+        return root
+            
